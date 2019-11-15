40a36249cbb3451a217f7d48c2a2ea43188d19a9
Josh Yap <joshua.c.yap@gmail.com>
2019-11-15 01:50:11 -0600

    starting to refactor for es6

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-function test() {
-    alert('working');
+let convertText = (i) => {
+    let inputTextValue = document.getElementById("inputText").value;
+    let outputTextValue;
+    
+    if (inputTextValue.length < 1) {
+        alert('enter a message to translate');
+    } else {
+        alert(inputTextValue);
+    }
+
+    outputTextValue = 'output';
+    document.getElementById("outputText").value = outputTextValue;
 }
 
-function reportText() {
-    let inputTextValue = document.getElementById("inputText").value;
-    alert(inputTextValue);
+let replaceVowels = str => {
+    for (let i=0; i<str.length; i++) {
+        if (str.charAt(i).toLowerCase() == "A" || str.charAt(i).toLowerCase() == "E" || str.charAt(i).toLowerCase() == "I" || str.charAt(i).toLowerCase() == "O" || str.charAt(i).toLowerCase() == "U") {
+            str.replace(str(i), "o");
+        }
+        return str;
+    }
 }
