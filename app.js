a1dcc6931c3e592b5b0d7568a2a78caa944fef57
Josh Yap <joshua.c.yap@gmail.com>
2019-11-18 23:58:33 -0600

    v2

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
-let convertText = (i) => {
-    let inputTextValue = document.getElementById("inputText").value;
-    let outputTextValue;
+// let convertText = (i) => {
+//     let inputTextValue = document.getElementById("inputText").value;
+//     let outputTextValue;
+    
+//     if (inputTextValue.length < 1) {
+//         alert('enter a message to translate');
+//     } else {
+//         alert(inputTextValue);
+//     }
+
+//     outputTextValue = 'output';
+//     document.getElementById("outputText").value = outputTextValue;
+// }
+
+let replaceVowels = inputTextValue => {
+    inputTextValue = document.getElementById("inputText").value;
     
     if (inputTextValue.length < 1) {
         alert('enter a message to translate');
     } else {
-        alert(inputTextValue);
-    }
-
-    outputTextValue = 'output';
-    document.getElementById("outputText").value = outputTextValue;
-}
-
-let replaceVowels = str => {
-    for (let i=0; i<str.length; i++) {
-        if (str.charAt(i).toLowerCase() == "A" || str.charAt(i).toLowerCase() == "E" || str.charAt(i).toLowerCase() == "I" || str.charAt(i).toLowerCase() == "O" || str.charAt(i).toLowerCase() == "U") {
-            str.replace(str(i), "o");
+        // alert(inputTextValue);
+        for (let i=0; i<inputTextValue.length; i++) {
+            if (inputTextValue.charAt(i).toLowerCase() == "a" || inputTextValue.charAt(i).toLowerCase() == "e" || inputTextValue.charAt(i).toLowerCase() == "i" || inputTextValue.charAt(i).toLowerCase() == "o" || inputTextValue.charAt(i).toLowerCase() == "u") {
+                inputTextValue.replace(inputTextValue(i), "o");
+            }
         }
-        return str;
+        alert(inputTextValue);
+        document.getElementById("outputText").value = inputTextValue;
     }
+    
+    // for (let i=0; i<inputTextValue.length; i++) {
+    //     if (inputTextValue.charAt(i).toLowerCase() == "A" || inputTextValue.charAt(i).toLowerCase() == "E" || inputTextValue.charAt(i).toLowerCase() == "I" || inputTextValue.charAt(i).toLowerCase() == "O" || inputTextValue.charAt(i).toLowerCase() == "U") {
+    //         inputTextValue.replace(inputTextValue(i), "o");
+    //     }
+    //     return inputTextValue;
+    // }
+    
 }
