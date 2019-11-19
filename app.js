1b47ba968797719ef4e9b13d529c71db2c7139cd
joshyap-civitas <joshyap@civitaslearning.com>
2019-11-19 17:53:41 -0600

    voice is working!

--- app.js
+++ app.js
@@ -1,6 +1,90 @@
 // https://mdn.github.io/web-speech-api/speak-easy-synthesis/
 // https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
 
+// init the speech synthesis api
+const synth = window.speechSynthesis;
+
+// grabbing the DOM elements
+const textForm = document.querySelector("form"); // ?
+const textInput = document.getElementById("inputText"); // ?
+const voiceSelect = document.getElementById("voice-select");
+const rate = document.getElementById("rate");
+const rateValue = document.getElementById("rate-value");
+const pitch = document.getElementById("pitch");
+const pitchValue = document.getElementById("pitch-value");
+
+// init the array of default fetched voices
+let voices =[];
+
+const getVoices = () => {
+    voices = synth.getVoices();
+    // console.log(voices);
+    voices.forEach(voice => {
+        const option = document.createElement('option');
+        option.textContent = voice.name + '(' + voice.lang + ')';
+        option.setAttribute('data-lang', voice.lang);
+        option.setAttribute('data-name', voice.name);
+        voiceSelect.appendChild(option);
+    });
+}
+
+getVoices();
+// this event isn't supported in firefox so have to call it here
+if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
+}
+
+const speak = () => {
+    // check if speaking already
+    if (synth.speaking) {
+        console.error('Already speaking...');
+        return;
+    }
+    if (textInput.value !== '') {
+        const speakText = new SpeechSynthesisUtterance(textInput.value);
+        
+        // run when done speaking
+        speakText.onend = e => {
+            console.log('Done speaking...');
+        }
+
+        // error check
+        speakText.onerror = e => {
+            console.error('Something went wrong :(');
+        }
+
+        // select voice
+        const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+        // loop through the voice options
+        voices.forEach(voice => {
+            if (voice.name === selectedVoice) {
+                speakText.voice = voice;
+            }
+        });
+
+        // set the rate and pitch
+        speakText.rate = rate.value;
+        speakText.pitch = pitch.value;
+
+        synth.speak(speakText);
+    }
+}
+
+
+// event listeners
+textForm.addEventListener('submit', e => {
+    e.preventDefault();
+    speak();
+    textInput.blur();
+});
+
+rate.addEventListener('change', e => rateValue.textContent = rate.value);
+pitch.addEventListener('change', e => pitchValue.textContent = pitch.value);
+
+voiceSelect.addEventListener('change', e => speak());
+
+// OG
 let replaceVowels = () => {
     inputTextValue = document.getElementById("inputText").value;
     var outputTextValue;
@@ -9,14 +93,13 @@ let replaceVowels = () => {
         alert('enter a message to translate');
     } else {
         outputTextValue = inputTextValue.replace(/o/g, "oo").replace(/a/g, "o").replace(/e/g, "o").replace(/i/g, "o").replace(/u/g, "o");
-        // alert(outputTextValue);
         document.getElementById("outputText").value = outputTextValue;
-        speak();
+        // speak();
     }   
 }
 
-let speak = () => {
-    var utterance = new SpeechSynthesisUtterance(outputTextValue);
-    window.speechSynthesis.speak(utterance);
-}
+// let speak = () => {
+//     var utterance = new SpeechSynthesisUtterance(outputTextValue);
+//     window.speechSynthesis.speak(utterance);
+// }
 
